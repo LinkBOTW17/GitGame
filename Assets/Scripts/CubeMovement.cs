d70428820ae6ddd79cc67b2d8d745dd26668ab8b
Laura Mandow <laurawodnam@gmail.com>
2020-09-01 15:47:27 +0200

    Improved the cube code
    Now we can see the cube moving

--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -4,17 +4,25 @@ using UnityEngine;
 
 public class CubeMovement : MonoBehaviour {
 
-	Rigidbody rg;
-	Vector3 destino;
+	public float MaxX = 5;
+	Vector3 velocidad;
 
 	// Use this for initialization
 	void Awake () {
-		rg = gameObject.GetComponent<Rigidbody> ();
-		destino = new Vector3 (5f, 0, 0);
+		velocidad = new Vector3 (5f, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Start () {
-		rg.MovePosition (transform.position + destino);
+		StartCoroutine (MoveCube ());
+	}
+
+	IEnumerator MoveCube()
+	{
+		while (gameObject.transform.position.x < MaxX) 
+		{
+			gameObject.transform.Translate (velocidad.x * Time.deltaTime, velocidad.y * Time.deltaTime, velocidad.z * Time.deltaTime);
+			yield return null;
+		}
 	}
 }
